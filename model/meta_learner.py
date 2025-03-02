6ef605cb4e29261e3abe66b4800ff6fcf32543c0
warma181 <11820537+warma181@user.noreply.gitee.com>
2025-03-02 17:38:02 +0800

    eps mec_loss lamda

--- model/meta_learner.py
+++ model/meta_learner.py
@@ -193,7 +193,7 @@ class MetaLearingClassification(nn.Module):
         self.m = 0
         self.d = 1000
         self.mu = 0
-        self.eps = 32
+        self.eps = 4
         self.eps_d = self.eps / self.d
         self.lamda = 0
 
@@ -351,7 +351,7 @@ class MetaLearingClassification(nn.Module):
         # print(self.lamda)
         loss_q = F.cross_entropy(logits, y)
         # args.m batch-size lamda_inv
-        mec_loss = self.mu * (loss_func(p, logits, self.lamda)) / self.m
+        mec_loss = self.mu * (loss_func(p, logits, self.lamda)) * 0.5 / self.m
         loss = loss_q - mec_loss * self.lamda
 
         return loss, logits,
